89adcb6f14fbaf8df5f7bf0cab16bf20c064f183
marchettogui <marchettoguigmail.com>
2021-06-29 20:30:02 -0300

    prefix do unknownCommand

--- commands/unknownCommand.js
+++ commands/unknownCommand.js
@@ -1,3 +1,4 @@
 module.exports = (Client, message) => {
-	message.channel.send({embed: {color: message.author.id % 0xFFFFFF, title: "Burrice!", description: "Ouve o que você tá falando, nem existe isso ai!\nVeja em $info para saber os comando existentes e como rolar seus dados"}});
+	Config = require("./config.json");
+	message.channel.send({embed: {color: message.author.id % 0xFFFFFF, title: "Burrice!", description: "Ouve o que você tá falando, nem existe isso ai!\nVeja em" + Config.prefix + "info para saber os comando existentes e como rolar seus dados"}});
 }
