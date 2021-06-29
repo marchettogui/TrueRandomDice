34d13038a6e9e7e5db40861dd4ecc4022c4db271
marchettogui <marchettoguigmail.com>
2021-06-29 20:45:44 -0300

    minor info and unknownCommand message changes

--- commands/unknownCommand.js
+++ commands/unknownCommand.js
@@ -1,4 +1,5 @@
 module.exports = (Client, message) => {
-	Config = require("./config.json");
-	message.channel.send({embed: {color: message.author.id % 0xFFFFFF, title: "Burrice!", description: "Ouve o que você tá falando, nem existe isso ai!\nVeja em" + Config.prefix + "info para saber os comando existentes e como rolar seus dados"}});
+	const Config = require("./config.json");
+
+	message.channel.send({embed: {color: message.author.id % 0xFFFFFF, title: "Burrice!", description: `Ouve o que você tá falando, nem existe isso ai!\nVeja em ${Config.prefix}info para saber os comando existentes e como rolar seus dados`}});
 }
